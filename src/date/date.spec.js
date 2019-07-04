afb14cedf2827ae1ed408d9f11ee3f5438e0f408
Michael <michael.isvy@online-pajak.com>
2019-07-04 08:41:52 +0800

    fixed bug in date test

--- src/date/date.spec.js
+++ src/date/date.spec.js
@@ -3,7 +3,6 @@ var moment = require("moment");
 test('should display current time', () => {
     let date1 = moment('2010-01-20');
     let date2 = moment('2010-01-21');
-    //expect(date2.isAfter(date1)).toBe(true);
-    expect(date2.getTime()).toBeGreaterThan(date1.getTime());
+    expect(date2.isAfter(date1)).toBe(true);
     
 });
