9f680bb6f28628216ed911b086fc42bd9a767ac3
Michael <michael.isvy@online-pajak.com>
2019-07-15 18:34:13 +0800

    added `array.spec.js` in order to show various ways to loop around an array

---
+++ src/collections/array.spec.js
@@ -0,0 +1,43 @@
+const sampleData = ['a','b','c','d','e'];
+
+describe('Experiments on Array functions', () => {
+    
+    test('should extract part of an array and expect value for one cell', () => {
+        let extractedData = sampleData.slice(1,3); // second element to 4th element
+        expect(extractedData[0]).toBe('b');
+        expect(extractedData).toEqual(['b', 'c']); // need to use toEqual so compares by value
+    });
+
+
+    test('should show that negative numbers start from the end of the index', () => {
+        let extractedData = sampleData.slice(-3,-1);
+        expect(extractedData).toEqual(['c', 'd']);
+    });
+
+    test('should use a for loop for an array', () => {
+        const numbers = [1,2,3,4];
+        let sumNumbers = 0;
+        for (let index = 0; index < numbers.length; index++) {
+            sumNumbers += numbers[index];           
+        }
+        expect(sumNumbers).toBe(10);
+    });
+
+    test('should reduce function inline', () => {
+        const numbers = [1,2,3,4];
+        let sumNumbers = numbers.reduce( (accumulator, currentValue) => accumulator + currentValue
+        );
+        expect(sumNumbers).toBe(10);
+    });
+
+    test('should reduce function with method block', () => {
+        const numbers = [1,2,3,4];
+        let sumNumbers = numbers.reduce (
+            function(accumulator, currentValue) {
+                return accumulator + currentValue;
+            }
+        )
+        expect(sumNumbers).toBe(10);
+        
+    });
+});
