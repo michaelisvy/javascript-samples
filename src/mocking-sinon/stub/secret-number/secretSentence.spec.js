f897ffd6a30204fdedf89ec61d581db22d933c82
Michael <michael.isvy@online-pajak.com>
2019-07-19 22:34:04 +0800

    added

--- src/mocking-sinon/stub/secret-number/secretSentence.spec.js
+++ src/mocking-sinon/stub/secret-number/secretSentence.spec.js
@@ -4,8 +4,14 @@ const secretNmber = require("./secretNumber");
 
 describe('Using Sinon with Stubs', () => {
     test('should call a Stub', () => {
-        sinon.stub(secretNmber, "getSecretNumber").returns(22);
+        let stub = sinon.stub(secretNmber, "getSecretNumber").returns(22);
         const result = secretSentence.getSecretSentence();
         expect(result).toBe("the secret was: 22");
+        stub.restore();
+    });
+
+    test('should call real function', () => {
+        const result = secretSentence.getSecretSentence();
+        expect(result).toBe("the secret was: 44");
     });
 });
