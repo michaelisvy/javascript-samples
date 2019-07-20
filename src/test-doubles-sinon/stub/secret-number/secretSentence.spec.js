050bda69a88e7a60555ec96c123a0d6d81ac2867
Michael <michael.isvy@online-pajak.com>
2019-07-20 17:54:33 +0800

    added sandbox test with

--- src/test-doubles-sinon/stub/secret-number/secretSentence.spec.js
+++ src/test-doubles-sinon/stub/secret-number/secretSentence.spec.js
@@ -2,13 +2,24 @@ const sinon = require("sinon");
 const secretSentence = require("./secretSentence");
 const secretNmber = require("./secretNumber");
 
+const sinonTestLib = require("sinon-test");
+const sinonTest = sinonTestLib(sinon);
+
+
 describe('Using Sinon with Stubs', () => {
-    test('should call a Stub', () => {
+    test('should call a Stub and restore in the end', () => {
         let stub = sinon.stub(secretNmber, "getSecretNumber").returns(22);
         const result = secretSentence.getSecretSentence();
         expect(result).toBe("the secret was: 22");
         stub.restore();
     });
+
+    test('should call a Stub in sandbox using sinon-test', sinonTest(() => {
+        let stub = sinon.stub(secretNmber, "getSecretNumber").returns(22);
+        const result = secretSentence.getSecretSentence();
+        expect(result).toBe("the secret was: 22");
+        stub.restore();
+    }));
 
     test('should call real function', () => {
         const result = secretSentence.getSecretSentence();
