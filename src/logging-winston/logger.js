822fbc2e4f052c3f295b91ab576dd64d73887b7a
Michael <michael.isvy@online-pajak.com>
2019-08-01 15:00:00 +0800

    winston: added the file name to log messages

--- src/logging-winston/logger.js
+++ src/logging-winston/logger.js
@@ -4,20 +4,25 @@
  * node src/logging-winston//logger.js
  */
 const {createLogger, format, transports} = require("winston");
+const path = require("path");
+
 
 const logger = createLogger({
-    level: 'debug',
-    format: format.combine(
-        format.colorize(),
-        format.timestamp({
-          format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-      ),
-    transports: [new transports.Console()]
-    // Alternatively, logging to a file:
-    // new transports.File({ filename })
+  level: 'debug',
+  format: format.combine(
+    format.label({ label: path.basename(process.mainModule.filename) }),
+    format.colorize(),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(
+      info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+    )
+  ),
+  transports: [new transports.Console()]
+  // Alternatively, logging to a file:
+  // new transports.File({ filename })
 });
+
+
 
 logger.debug("hello debug");
 logger.info("hello info");
