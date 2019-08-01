b787d998bbb78cf50c76bd502fa70d842e11f4b9
Michael <michael.isvy@online-pajak.com>
2019-08-01 14:51:50 +0800

    added one moment.js example

--- src/date/date.spec.js
+++ src/date/date.spec.js
@@ -1,7 +1,14 @@
 const moment = require("moment");
 
-test('should display current time', () => {
-    const date1 = moment('2010-01-20');
-    const date2 = moment('2010-01-21');
+const date1 = moment('2010-01-20');
+const date2 = moment('2010-01-21');
+
+test('should check that one date is after another', () => {
     expect(date2.isAfter(date1)).toBe(true);    
+});
+
+test('should compare year/month/day', () => {
+    expect(date1.year()).toEqual(2010);
+    expect(date1.month()).toEqual(0); // Month starts at 0, not 1
+    expect(date1.date()).toEqual(20);
 });
