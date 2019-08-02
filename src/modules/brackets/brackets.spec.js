b3f4df1e6593db23d359465a9e2c285d99ae9412
Michael <michael.isvy@online-pajak.com>
2019-08-02 17:17:32 +0800

    added one module with names

--- src/modules/brackets/brackets.spec.js
+++ src/modules/brackets/brackets.spec.js
@@ -1,13 +1,17 @@
 const greet = require("./example1"); // exported with no brackets
-
 const {hello1, hello2} = require("./example2"); // both functions have been exported with brackets
+const {helloInstance3} = require("./example3");
 
 describe('Name of the group', () => {
-    test('should ', () => {
+    test('example1', () => {
         expect(greet()).toEqual("hello example1");
     });
-    test('should ', () => {
+    test('example2', () => {
         expect(hello1()).toEqual("hello1 example2");
         expect(hello2()).toEqual("hello2 example2");
+    });
+
+    test('example3', () => {
+        expect(helloInstance3.greetClass()).toEqual("hello");
     });
 });
---
+++ src/modules/brackets/example3.js
@@ -0,0 +1,7 @@
+class Hello {
+    greetClass() {
+        return "hello";
+    }
+}
+const helloInstance3 = new Hello();
+module.exports = {helloInstance3}
