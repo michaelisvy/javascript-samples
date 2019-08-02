bb65f818c7d51cf4640178b3c2f4a6afe411d641
Michael <michael.isvy@online-pajak.com>
2019-08-02 17:26:00 +0800

    made dependency injection example more dynamic

--- src/dependency-injection/accountRepositoryFactory.js
+++ src/dependency-injection/accountRepositoryFactory.js
@@ -1,14 +1,8 @@
-const accountRepository1 = require("./accountRepository1");
-const accountRepository2 = require("./accountRepository2");
 const config = require("./config.json");
+const accountRepository = require("./" + config.repository); 
 
 function getAccountRepository() {
-    // as of now, it is not a singleton
-    if(config.repository === "repo1") {
-        return accountRepository1; 
-    } else {
-        return accountRepository2;
-    }
+    return accountRepository;
 }
 
 module.exports = {getAccountRepository};
