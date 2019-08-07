3785881d5353952951ed845cc47bae6a92b7bc42
Michael <michael.isvy@online-pajak.com>
2019-08-07 09:20:57 +0800

    moved stub.restore() into afterEach block

--- src/test-doubles-sinon/stub/secret-number/secretSentence.spec.js
+++ src/test-doubles-sinon/stub/secret-number/secretSentence.spec.js
@@ -4,28 +4,31 @@ const secretNmber = require("./secretNumber");
 const sinon = require('sinon');
 
 describe('Using Sinon with Stubs', () => {
+    let stub;
+
+    afterEach(() => {
+        stub.restore();
+      });
+
     it('should call a Stub', () => {
-        const stub = sinon.stub(secretNmber, "getSecretNumber").returns(22);
+        stub = sinon.stub(secretNmber, "getSecretNumber").returns(22);
         const result = secretSentence.getSecretSentence();
-        stub.restore(); // restore should always happen before expect
+         // restore should always happen before expect
         expect(result).toBe("the secret was: 22");
     });
 
     it('should call a Stub and use calledOnce', () => {
-        const stub = sinon.stub(secretNmber, "getSecretNumber").returns(24);
+        stub = sinon.stub(secretNmber, "getSecretNumber").returns(24);
         const result = secretSentence.getSecretSentence(); 
         sinon.assert.calledOnce(stub);
-        stub.restore();
         expect(result).toBe("the secret was: 24");
     });
 
     it('should call a Stub that throws an error', () => {
-        const stub = sinon.stub(secretNmber, "getSecretNumber").throws(new Error("my secret error"));
+        stub = sinon.stub(secretNmber, "getSecretNumber").throws(new Error("my secret error"));
         let callFunction = function() {secretSentence.getSecretSentence()}; 
         // needs to be wrapped in a function as we're using expect().toThrow()
         expect(callFunction).toThrow(new Error ("my secret error"));
         sinon.assert.calledOnce(stub);
-        stub.restore();
-
     });
 });
