39ebfbb76df296e875b32778aee72c856ab4bd45
Michael <michael.isvy@online-pajak.com>
2019-08-07 16:59:55 +0800

    used spread operator for a function

--- src/collections/arraySpread.spec.js
+++ src/collections/arraySpread.spec.js
@@ -8,8 +8,8 @@ describe('Experiments on other Array functions', () => {
 
     test('should use spread operator', () => {
         const numbers1 = [1,2];
-        const numbers2 = [3,4];
-        const numbers3 = [...numbers1, ...numbers2, 5, 6]; // merges 3 arrays together
+        const numbers2 = () => { return [3,4] };
+        const numbers3 = [...numbers1, ...numbers2(), 5, 6]; // merges 3 arrays together
         expect(numbers3).toEqual([1,2,3,4,5,6]);
     });
 
