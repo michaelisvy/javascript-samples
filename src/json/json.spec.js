007b1dd795d7c6704fea582e12ce0f2089941cf5
Michael <michael.isvy@online-pajak.com>
2019-08-08 12:06:33 +0800

    added one example to show how to insert an attribute in a JSON object

--- src/json/json.spec.js
+++ src/json/json.spec.js
@@ -1,12 +1,24 @@
+
+
 describe('Tests on Json objects', () => {
-    test('should write into a JSON object (JavaScript syntax)', () => {
+    test('should write into a JSON object', () => {         
         const song = { title: "Little Wing",
-                    artist: "Jimi Hendrix",
-                    year: 1967,
-                    price: 5
-        }      
+            artist: "Jimi Hendrix",
+            year: 1967,
+            price: 5
+        }         
         song.price = 0.8 * song.price; // 20% discount
         expect(song.price).toBe(4);
+    });
+
+    test('should add an attribute to a JSON object', () => {  
+        const song = { title: "Little Wing",
+            artist: "Jimi Hendrix",
+            year: 1967,
+            price: 5
+        }            
+        song.duration = "60s"; // attribute is added to the end of the JSON object
+        expect(song.duration).toBe("60s");
     });
 
     test('should read from a configuration file', () => {
