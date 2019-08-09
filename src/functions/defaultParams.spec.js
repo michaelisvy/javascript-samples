313714d369e0e1a960e448dd0f9b78368d677dee
Michael <michael.isvy@online-pajak.com>
2019-08-09 09:37:49 +0800

    added default example for an array

--- src/functions/defaultParams.spec.js
+++ src/functions/defaultParams.spec.js
@@ -2,7 +2,16 @@ function sayHello(name = "Bob") {
     return `hello ${name}`;
 }
 
-test('should ', () => {
+function sayMultipleHellos(names = ["Bob"]) {
+    return `hello ${names}`;
+}
+
+test('should test default parameters', () => {
     expect(sayHello("Sam")).toEqual("hello Sam");
     expect(sayHello()).toEqual("hello Bob");
+});
+
+test('should test default parameters for an array', () => {
+    expect(sayMultipleHellos(["Sam"])).toEqual("hello Sam");
+    expect(sayMultipleHellos()).toEqual("hello Bob");
 });
