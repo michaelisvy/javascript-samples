91183f190c5c07c50e1cd4c995434b23f16c9c86
Michael <michael.isvy@online-pajak.com>
2019-08-09 09:47:33 +0800

    default parameters don't apply when using spread operator

--- src/functions/defaultParams.spec.js
+++ src/functions/defaultParams.spec.js
@@ -6,12 +6,25 @@ function sayMultipleHellos(names = ["Bob"]) {
     return `hello ${names}`;
 }
 
+function saySpreadHellos(...names) { // default parameters do not work when using the Spread operator
+    console.log(names);
+    if (names.length === 0) {
+        return "hello Bob";
+    }
+    return `hello ${names}`;
+}
+
 test('should test default parameters', () => {
     expect(sayHello("Sam")).toEqual("hello Sam");
     expect(sayHello()).toEqual("hello Bob");
 });
 
 test('should test default parameters for an array', () => {
-    expect(sayMultipleHellos(["Sam"])).toEqual("hello Sam");
+    expect(sayMultipleHellos(["Sam", "John"])).toEqual("hello Sam,John");
     expect(sayMultipleHellos()).toEqual("hello Bob");
+});
+
+test('should test default parameters for an array using spread operator', () => {
+    expect(saySpreadHellos("Sam", "John")).toEqual("hello Sam,John");
+    expect(saySpreadHellos()).toEqual("hello Bob");
 });
