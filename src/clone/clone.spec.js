5659f36de252545b566ebb0b23ed313c93017568
Michael <michael.isvy@online-pajak.com>
2019-08-21 17:05:11 +0800

    added deep clone example with Lodash

---
+++ src/clone/clone.spec.js
@@ -0,0 +1,38 @@
+const clonedeep = require('lodash.clonedeep')
+
+describe('Experiments on Array clones', () => {
+
+    test('should copy an object', () => {
+        let s1 = new Song("Little Wing");
+        let s2 = Object.assign({}, s1);
+        s2.title = "Hey Joe";
+
+        expect(s1.title).toEqual("Little Wing");
+        expect(s2.title).toEqual("Hey Joe");        
+    });
+    
+    test('should clone a simple array using slice', () => {
+        const originalData = ['a','b','c','d','e'];
+        const cloneData = originalData.slice(); 
+        cloneData[0] = 'aa';
+        expect(originalData).toEqual(['a','b','c','d','e']);
+        expect(cloneData).toEqual(['aa','b','c','d','e']);
+    });
+
+    test('should make a deep clone', () => {
+        let originalSongs = [new Song("Little Wing"), new Song("Hey Joe")];
+        let copySongs = clonedeep(originalSongs);
+        copySongs[0].title = "All Along of the Watchtower";
+        console.log(copySongs);
+
+        expect(originalSongs[0].title).toEqual("Little Wing");
+        expect(copySongs[0].title).toEqual("All Along of the Watchtower");
+    });
+});
+
+class Song {
+    constructor(title) {
+        this.title = title;
+    }
+}
+    
